0ecbd617d2c3a72d06f7a3776b07913351a852cc
P-A-R-T-H-A <partha.cse.0@gmail.com>
2025-06-28 01:13:32 +0600

    remove vapid gen

--- main.go
+++ main.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	_ "bondi-push-notification/routers"
-	"fmt"
 
-	"github.com/SherClockHolmes/webpush-go"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func main() {
-	privateKey, publicKey, _ := webpush.GenerateVAPIDKeys()
-	fmt.Println(privateKey)
-	fmt.Println(publicKey)
+	// privateKey, publicKey, _ := webpush.GenerateVAPIDKeys()
+	// fmt.Println(privateKey)
+	// fmt.Println(publicKey)
 	beego.Run()
 
 }
